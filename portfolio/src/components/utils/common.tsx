ab798eebdc6fdbd13c2924ef584edfefcf6c7f70
demeshFernando <demeshfernando2000@gmail.com>
2025-11-03 21:46:16 +0530

    portfolio Model

---
+++ portfolio/src/components/utils/common.tsx
@@ -0,0 +1,30 @@
+type MultiPropsType<T extends Record<string ,unknown>, S extends keyof T> = {
+    identifier: 'multi',
+    model: T,
+    binders: {
+        [K in S]: T[K];
+    }
+};
+
+type KeyValuePropsType<T extends Record<string, unknown>, S extends keyof T> = {
+    identifier: 'single',
+    model: T,
+    key: S,
+    value: T[S],
+};
+
+type ModelProps<T extends Record<string, unknown>, S extends keyof T> = MultiPropsType<T, S> | KeyValuePropsType<T, S>;
+
+export function bindToModel<T extends Record<string, unknown>, S extends keyof T>(props: ModelProps<T, S>): T{
+    //if we got multi binder
+    if(props.identifier === 'single') {
+        return {
+            ...props.model,
+            [props.key]: props.value,
+        };
+    }
+    return {
+        ...props.model,
+        ...props.binders,
+    };
+}
